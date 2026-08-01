9377152b5fe4cfd7fc24eea53eae71616493eb19
naveen-kstb <naveenkumarst11@gmail.com>
2026-08-01 18:05:58 +0530

    CGI_L1_Interview_Code_2026.py

---
+++ CGI_L1_Interview_2026.py
@@ -0,0 +1,26 @@
+# WAP code to print the below format
+# *
+# **
+# ***
+# ****
+rows = 4
+for i in range(1, rows + 1):
+    print("*" * i)
+
+# robot - -test "Login Test" Tests / login.robot - v - m
+#
+# Open Browser  ${URL}
+#
+# ${str1} set variable Hello
+# ${str2} set variable Python
+#
+# Should be Equal As Strings ${str1}    ${str2}
+#
+# ** *Settings ** *
+#
+# ** *Variables ** **
+#
+# ** *Test
+# Case ** *
+#
+# ** *Keyword ** *
